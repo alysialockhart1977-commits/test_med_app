e26657867cd59aabfcb9679a24dcaadaa620b7c1
alysialockhart1977-commits <alysialockhart1977@gmail.com>
2026-02-23 15:02:30 -0500

    Continue to create a React component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// Import React
+import React from "react";
+
+// Import CSS
+import "./App.css";
+
+// Import components for routing from react-router-dom library
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Import Navbar
+import Navbar from './components/Navbar';  // adjust path if needed
+import Landing_Page from './components/Landing_Page';
+import Login from './components/Login';
+import Sign_Up from './components/Sign_Up';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />  {/* Navation Bar visible on all pages */}
+      <Routes>
+        {/* Your Route components */}
+        <Route path="/" element={<Landing_Page />} />
+        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Sign_Up />} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
-
+// Export the App component as the default export
 export default App;
