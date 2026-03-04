ff98b5724fd5450d0e6dc2645104d318b9da2373
alysialockhart1977-commits <alysialockhart1977@gmail.com>
2026-03-04 14:10:42 -0500

    Modified LandingPage.js (deleted duplicate .JS) and mo LandingPage.css

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import React from "react";
 // Import CSS
 import "./App.css";
 
-// Import components for routing from react-router-dom library
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Import routing
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Import Navbar
-import Navbar from './Components/Navbar/Navbar';  // adjust path if needed
-import LandingPage from './Components/Landing_Page/LandingPage';
-import Login from './Components/Login/Login';
-import SignUp from './Components/Sign_Up/Sign_Up';
+// Import components
+import Navbar from "./Components/Navbar/Navbar";
+import LandingPage from "./Components/Landing_Page/LandingPage";
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/Sign_Up/Sign_Up";
+
+// Temporary page
 const Appointments = () => <h1>Appointments Page</h1>;
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />  {/* Navation Bar visible on all pages */}
+      {/* Navbar appears at top of every page */}
+      <Navbar />
+
       <Routes>
-        {/* Your Route components */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Sign_Up />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/appointments" element={<Appointments />} />
-        
       </Routes>
     </BrowserRouter>
   );
 }
-// Export the App component as the default export
+
 export default App;
