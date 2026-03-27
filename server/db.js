d58b359dd7537b79fdfeb56cca1143b34e04bc84
alysialockhart1977-commits <alysialockhart1977@gmail.com>
2026-03-27 17:26:18 -0400

    Modify to fix errors

--- server/db.js
+++ server/db.js
@@ -1,28 +1,14 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:h4XCgQcg76bK9lPSdqAnC2Z5@172.21.114.21:27017";
 
-const connectToMongo = async (retryCount) => {
-    const MAX_RETRIES = 3;
-    const count = retryCount ?? 0;
-    try {
-        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
-        console.info('Connected to Mongo Successfully')
+const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/test_med";
 
-        return;
-    } catch (error) {
-        console.error(error);
-
-        const nextRetryCount = count + 1;
-
-        if (nextRetryCount >= MAX_RETRIES) {
-            throw new Error('Unable to connect to Mongo!');
-        }
-
-        console.info(`Retrying, retry count: ${nextRetryCount}`)
-
-        return await connectToMongo(nextRetryCount);
-
-    }
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
 };
 
 module.exports = connectToMongo;
